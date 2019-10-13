81fe548692baa4d3d6b6bbdf2eed9a47e209daff
George Buckenham <vtwentyone@gmail.com>
2019-10-13 11:03:35 +0100

    finished??

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,81 @@
+extern crate glob;
 extern crate structopt;
 extern crate usvg;
 
 mod stripper;
 
+use glob::glob;
+use std::fs::File;
+use std::io::prelude::*;
+use std::option::Option;
+use std::result::Result;
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
     /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+    path: String,
+
+    #[structopt(parse(from_os_str), short = "o", long = "output-dir")]
+    output_dir: Option<std::path::PathBuf>,
+
+    /// the value to send for M3 commands when lifting the pen
+    #[structopt(
+        short = "u",
+        long = "pen-up",
+        default_value = "0",
+        env = "PEN_UP_HEIGHT"
+    )]
+    pen_up_height: f64,
+
+    /// the value to send for M3 commands when dropping the pen
+    #[structopt(
+        short = "d",
+        long = "pen-down",
+        default_value = "100",
+        env = "PEN_DOWN_HEIGHT"
+    )]
+    pen_down_height: f64,
 }
 
 fn main() {
     let args = Cli::from_args();
 
-    const xml_opt: usvg::XmlOptions = usvg::XmlOptions {
+    const XML_OPT: usvg::XmlOptions = usvg::XmlOptions {
         use_single_quote: true,
         indent: usvg::XmlIndent::Spaces(4),
         attributes_indent: usvg::XmlIndent::Spaces(4),
     };
-    let rtree = usvg::Tree::from_file(&args.path, &usvg::Options::default()).unwrap();
-    let stripped_tree = stripper::strip(&rtree, xml_opt);
-    print!("{}", rtree.to_string(xml_opt));
+
+    let params = stripper::Params {
+        pen_up_height: args.pen_up_height,
+        pen_down_height: args.pen_down_height,
+    };
+
+    for p in glob(&args.path).unwrap().filter_map(Result::ok) {
+        let rtree = usvg::Tree::from_file(&p, &usvg::Options::default()).unwrap();
+        let (stripped_svg, gcode) = stripper::strip(&rtree, XML_OPT, &params);
+
+        let out_dir = args
+            .output_dir
+            .clone()
+            .unwrap_or(p.parent().unwrap().to_path_buf());
+
+        let mut stripped_svg_path = out_dir.clone();
+        stripped_svg_path.push(p.with_extension("stripped.svg").file_name().unwrap());
+        File::create(&stripped_svg_path)
+            .unwrap()
+            .write_all(stripped_svg.as_bytes())
+            .unwrap();
+
+        let mut gcode_path = out_dir.clone();
+        gcode_path.push(p.with_extension("gcode").file_name().unwrap());
+        File::create(&gcode_path)
+            .unwrap()
+            .write_all(gcode.as_bytes())
+            .unwrap();
+    }
+
+    //print!("{}", rtree.to_string(xml_opt));
 }
--- src/stripper.rs
+++ src/stripper.rs
@@ -1,5 +1,7 @@
+extern crate kurbo;
 extern crate usvg;
 
+use kurbo::{CubicBez, Line, ParamCurve, ParamCurveArclen, Point};
 use std::ops::Deref;
 use xmlwriter::XmlWriter;
 
@@ -8,14 +10,24 @@ struct State {
     gcode: String,
     pen_up: bool,
     current_svg_path: String,
+    current_loc: Point,
+    params: Params,
 }
 
-pub fn strip(tree: &usvg::Tree, xml_opt: usvg::XmlOptions) {
+#[derive(Clone)]
+pub struct Params {
+    pub pen_up_height: f64,
+    pub pen_down_height: f64,
+}
+
+pub fn strip(tree: &usvg::Tree, xml_opt: usvg::XmlOptions, params: &Params) -> (String, String) {
     let mut state = State {
         svg: XmlWriter::new(xml_opt),
         gcode: String::default(),
         current_svg_path: String::default(),
         pen_up: false,
+        current_loc: Point::ORIGIN,
+        params: (*params).clone(),
     };
 
     state.svg.start_element("svg");
@@ -41,21 +53,24 @@ pub fn strip(tree: &usvg::Tree, xml_opt: usvg::XmlOptions) {
     for child in tree.root().children() {
         match child.borrow().deref() {
             usvg::NodeKind::Path(ref path) => {
-                println!("{:?}, {}", path.data, path.transform);
+                //println!("{:?}, {}", path.data, path.transform);
 
                 state.svg.start_element("path");
                 state.svg.write_attribute("stroke", "black");
                 state.svg.write_attribute("fill", "none");
 
                 state.current_svg_path = String::default();
+                let a = |&x, &y| path.transform.apply(x, y);
 
                 for p in path.data.iter() {
                     match p {
                         usvg::PathSegment::MoveTo { x, y } => {
-                            state = move_to(state, x, y);
+                            let (tx, ty) = a(x, y);
+                            state = move_to(state, &tx, &ty);
                         }
                         usvg::PathSegment::LineTo { x, y } => {
-                            state = line_to(state, x, y);
+                            let (tx, ty) = a(x, y);
+                            state = line_to(state, &tx, &ty);
                         }
                         usvg::PathSegment::CurveTo {
                             x1,
@@ -65,7 +80,10 @@ pub fn strip(tree: &usvg::Tree, xml_opt: usvg::XmlOptions) {
                             x,
                             y,
                         } => {
-                            state = curve_to(state, x1, y1, x2, y2, x, y);
+                            let (tx1, ty1) = a(x1, y1);
+                            let (tx2, ty2) = a(x2, y2);
+                            let (tx, ty) = a(x, y);
+                            state = curve_to(state, &tx1, &ty1, &tx2, &ty2, &tx, &ty);
                         }
                         usvg::PathSegment::ClosePath => {}
                     }
@@ -78,25 +96,42 @@ pub fn strip(tree: &usvg::Tree, xml_opt: usvg::XmlOptions) {
             _ => {}
         }
     }
-    println!("{}\n{}", state.gcode, state.svg.end_document());
+
+    return (state.svg.end_document(), state.gcode);
+    //println!("{}\n{}", state.gcode, state.svg.end_document());
 }
 
 fn move_to<'a>(mut state: State, x: &f64, y: &f64) -> State {
     state = maybe_pen_up(state);
     state.gcode.push_str(&format!("G0 X{} Y{}\n", x, y));
     state.current_svg_path.push_str(&format!("M {} {} ", x, y));
+    state.current_loc = Point::new(*x, *y);
     return state;
 }
 
 fn curve_to(mut state: State, x1: &f64, y1: &f64, x2: &f64, y2: &f64, x: &f64, y: &f64) -> State {
-    state = maybe_pen_down(state);
-    state.gcode.push_str(&format!(
-        "G5 I{} J{} P{} Q{} X{} Y{}\n",
-        x1, y1, x2, y2, x, y
-    ));
-    state
-        .current_svg_path
-        .push_str(&format!("C {} {} {} {} {} {} ", x1, y1, x2, y2, x, y));
+    // state = maybe_pen_down(state);
+
+    let bez = CubicBez::new(
+        state.current_loc,
+        Point::new(*x1, *y1),
+        Point::new(*x2, *y2),
+        Point::new(*x, *y),
+    );
+
+    for l in bezier_to_lines(bez) {
+        state = line_to(state, &l.end().x, &l.end().y);
+    }
+
+    // state.gcode.push_str(&format!(
+    //     "G5 I{} J{} P{} Q{} X{} Y{}\n",
+    //     x1, y1, x2, y2, x, y
+    // ));
+    // state
+    //     .current_svg_path
+    //     .push_str(&format!("C {} {} {} {} {} {} ", x1, y1, x2, y2, x, y));
+
+    // state.current_loc = Point::new(*x, *y);
     return state;
 }
 
@@ -104,12 +139,16 @@ fn line_to(mut state: State, x: &f64, y: &f64) -> State {
     state = maybe_pen_down(state);
     state.gcode.push_str(&format!("G1 X{} Y{} F10000\n", x, y));
     state.current_svg_path.push_str(&format!("L {} {} ", x, y));
+
+    state.current_loc = Point::new(*x, *y);
     return state;
 }
 
 fn maybe_pen_up(mut state: State) -> State {
     if !state.pen_up {
-        state.gcode.push_str("M3 S0\n");
+        state
+            .gcode
+            .push_str(&format!("M3 S{}\n", &state.params.pen_up_height));
         state.pen_up = true;
     }
     return state;
@@ -117,10 +156,27 @@ fn maybe_pen_up(mut state: State) -> State {
 
 fn maybe_pen_down(mut state: State) -> State {
     if state.pen_up {
-        state.gcode.push_str("M3 S100\n");
+        state
+            .gcode
+            .push_str(&format!("M3 S{}\n", &state.params.pen_down_height));
         state.pen_up = false;
     }
     return state;
+}
+
+fn bezier_to_lines(bez: CubicBez) -> Vec<Line> {
+    let count = i64::max(bez.arclen(1.).floor() as i64, 4);
+    let mut pos = bez.start();
+    let mut lines: Vec<Line> = Vec::new();
+
+    for i in 1..=count {
+        let t = i as f64 / count as f64;
+        let new_pos = bez.eval(t);
+        lines.push(Line::new(pos, new_pos));
+        pos = new_pos;
+    }
+
+    return lines;
 }
 
 // fn recurse_strip(parent: &usvg::Node, xml: &mut XmlWriter) {
